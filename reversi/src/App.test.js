a56281017fa617d11ea5d87f1c7060368687edc3
kou-practice <kouyou.kuroshita@artemis-tech.js>
2020-12-10 00:33:47 +0900

    初めてのJest

---
+++ reversi/src/App.test.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { render, unmountComponentAtNode } from 'react-dom';
+import { act } from "react-dom/test-utils";
+
+import { Square } from './App';
+
+let container = null;
+beforeEach(() => {
+    container = document.createElement("div");
+    document.body.appendChild(container);
+});
+
+afterEach(() => {
+    unmountComponentAtNode(container);
+    container.remove();
+    container = null;
+});
+
+it("renders black or white", () => {
+    act(() => {
+        render(<Square />, container);
+    });
+    expect(container.getElementsByTagName('p')[0].className).toBe("");
+});
